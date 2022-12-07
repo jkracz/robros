03a80f517639258a9ba34142d3785f1233f64930
jkracz <joseph.kracz@gmail.com>
2022-12-06 21:30:35 -0800

    removed old commented code

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,45 +36,4 @@ function App() {
   );
 }
 
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       robots: [],
-//       searchfield: "",
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((res) => res.json())
-//       .then((users) => {
-//         this.setState({ robots: users });
-//       });
-//   }
-
-//   onSearchChange = (event) => {
-//     this.setState({ searchfield: event.target.value });
-//   };
-
-//   render() {
-//     const filteredRobots = this.state.robots.filter((robots) => {
-//       return robots.name
-//         .toLowerCase()
-//         .includes(this.state.searchfield.toLowerCase());
-//     });
-//     return (
-//       <div className="tc">
-//         <h1 className="f1">RoBros</h1>
-//         <SearchBox searchChange={this.onSearchChange} />
-//         <Scroll>
-//           <ErrorBoundary>
-//             <CardList robots={filteredRobots} />
-//           </ErrorBoundary>
-//         </Scroll>
-//       </div>
-//     );
-//   }
-// }
-
 export default App;
